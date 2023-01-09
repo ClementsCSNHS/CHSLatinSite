e1e0280a717de25572b40e5e4afcfdd1a94b44ee
Leo Zhang <leoyzhang06@gmail.com>
2023-01-09 14:26:14 -0600

    home page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,25 @@ import styles from './Home.module.scss';
 export default function Home() {
   return (
     <div className={styles.Home}>
-      <h1 className={styles.title}>CHS Latin Website</h1>
+      <section id='domus' className={styles.domus}>
+        <h1 className={styles.domus_title}>CHS Latin Website</h1>
+      </section>
+
+      <section id='about' className={styles.about}>
+        About
+      </section>
+
+      <section id='resources' className={styles.resources}>
+        Resources
+      </section>
+
+      <section id='calendar' className={styles.calendar}>
+        Calendar
+      </section>
+
+      <section id='gallery' className={styles.gallery}>
+        Gallery
+      </section>
     </div>
   );
 }
