c6071521f953fec22648ce62dc3f4ac3dd43e383
Leo Zhang <leoyzhang06@gmail.com>
2023-01-10 10:30:47 -0600

    removed scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from './pages/Home';
 
 export default function App() {
   return (
-    <InfiniteScroll>
+    // <InfiniteScroll>
       <Home />
-    </InfiniteScroll>
+    // </InfiniteScroll>
   )
 }
