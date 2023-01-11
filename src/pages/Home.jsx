28a6ebe99e65b7622e882cf8d854cdd974214689
YottaYocta <chubbean.rq@gmail.com>
2023-01-11 10:35:33 -0600

    background and window

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
-import { useRef, useEffect, useState } from 'react';
-import Navigation from '../components/Navigation';
-import styles from './Home.module.scss';
+import { useRef, useEffect, useState } from "react";
+import Navigation from "../components/Navigation";
+import styles from "./Home.module.scss";
 
 export default function Home() {
-  const [currSection, setCurrSection] = useState('domus');
+  const [currSection, setCurrSection] = useState("domus");
 
   const refs = {
     domus: useRef(),
     about: useRef(),
     resources: useRef(),
     calendar: useRef(),
-    gallery: useRef()
+    gallery: useRef(),
   };
 
   useEffect(() => {
     const onScroll = () => {
       let curr;
-      for(const x in refs) {
+      for (const x in refs) {
         let bounds = refs[x].current.getBoundingClientRect();
-        if(bounds.top <= 0 && bounds.bottom >= 0) {
+        if (bounds.top <= 0 && bounds.bottom >= 0) {
           curr = x;
         }
       }
@@ -30,25 +30,47 @@ export default function Home() {
 
   return (
     <div className={styles.Home}>
+      <svg
+        fill="black"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid slice"
+        className={styles.window}
+      >
+        <mask id="myMask">
+          <rect x="0" y="0" width="100%" height="100%" fill="white" />
+          <rect x="40" y="40" width="20" height="20" fill="black"></rect>
+        </mask>
+
+        <rect
+          x="0"
+          y="0"
+          width="100%"
+          height="100%"
+          mask="url(#myMask)"
+          fill="black"
+          className={styles.window}
+        />
+      </svg>
+
       <Navigation selected={currSection} />
 
-      <section ref={refs.domus} id='domus' className={styles.domus}>
+      <section ref={refs.domus} id="domus" className={styles.domus}>
         <h1 className={styles.domus_title}>CHS Latin Website</h1>
       </section>
 
-      <section ref={refs.about} id='about' className={styles.about}>
+      <section ref={refs.about} id="about" className={styles.about}>
         About
       </section>
 
-      <section ref={refs.resources} id='resources' className={styles.resources}>
+      <section ref={refs.resources} id="resources" className={styles.resources}>
         Resources
       </section>
 
-      <section ref={refs.calendar} id='calendar' className={styles.calendar}>
+      <section ref={refs.calendar} id="calendar" className={styles.calendar}>
         Calendar
       </section>
 
-      <section ref={refs.gallery} id='gallery' className={styles.gallery}>
+      <section ref={refs.gallery} id="gallery" className={styles.gallery}>
         Gallery
       </section>
     </div>
